762d2acb856065a116644e54769d4d10074c392d
ViktoriiaRepo <samarityankav@gmail.com>
2024-03-10 20:41:54 +0200

    Add files

---
+++ src/App.jsx
@@ -0,0 +1,229 @@
+import { useState } from 'react';
+
+import 'bulma/css/bulma.css';
+import './App.scss';
+import cn from 'classnames';
+
+import peopleFromServer from '../public/api/people.json';
+
+const SORT_FIELD_ALPHABETICALLY = 'Sorted alphabetically by name';
+const SORT_FIELD_BY_AGE = 'Sorted by age';
+function getPreparedPeople(people, filterField, sortField) {
+  let visiblePeople = [...people];
+
+  if (filterField.query) {
+    visiblePeople = visiblePeople.filter((person) => {
+      const { name } = person;
+      const foundPerson = name
+        .toLowerCase()
+        .trim()
+        .includes(filterField.query.trim().toLowerCase());
+
+      return foundPerson;
+    });
+  }
+
+  if (filterField.gender !== 'all') {
+    visiblePeople = visiblePeople.filter(
+      (person) => person.sex === filterField.gender
+    );
+  }
+
+  if (sortField.sortType === SORT_FIELD_ALPHABETICALLY) {
+    visiblePeople = visiblePeople.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  if (sortField.sortType === SORT_FIELD_BY_AGE) {
+    visiblePeople = visiblePeople.sort((a, b) => a.born - b.born);
+  }
+
+  if (sortField.isReversed) {
+    console.log('reverse', sortField.isReversed);
+    visiblePeople.reverse();
+  }
+
+  return visiblePeople;
+}
+
+function App() {
+  // #region People Selection Logic
+  const [selectedPeople, setSelectedPerson] = useState([]);
+  const [filterField, setFilterField] = useState({
+    query: '',
+    gender: 'all',
+  });
+
+  const [sortField, setSortField] = useState({
+    sortType: '',
+    isReversed: false,
+  });
+
+  const visiblePeople = getPreparedPeople(
+    peopleFromServer,
+    filterField,
+    sortField
+  );
+
+  const isSelected = ({ slug }) => selectedPeople.some((p) => p.slug === slug);
+
+  const addPerson = (person) => {
+    setSelectedPerson([...selectedPeople, person]);
+  };
+
+  const removePerson = (person) => {
+    setSelectedPerson(selectedPeople.filter((p) => p.slug !== person.slug));
+  };
+
+  return (
+    <div className='box'>
+      <div className='block'>
+        <div className='buttons has-addons'>
+          <button
+            type='button'
+            className={cn('button', {
+              'is-info': filterField.gender === 'all',
+            })}
+            onClick={() =>
+              setFilterField((prevState) => ({
+                ...prevState,
+                ...filterField,
+                gender: 'all',
+              }))
+            }
+          >
+            All
+          </button>
+          <button
+            type='button'
+            className={cn('button', { 'is-info': filterField.gender === 'm' })}
+            onClick={() =>
+              setFilterField((prevState) => ({
+                ...prevState,
+                ...filterField,
+                gender: 'm',
+              }))
+            }
+          >
+            Man
+          </button>
+          <button
+            type='button'
+            className={cn('button', { 'is-info': filterField.gender === 'f' })}
+            onClick={() =>
+              setFilterField((prevState) => ({
+                ...prevState,
+                ...filterField,
+                gender: 'f',
+              }))
+            }
+          >
+            Women
+          </button>
+        </div>
+
+        <div className='buttons has-addons'>
+          <button
+            type='button'
+            className={cn('button', {
+              'is-info': sortField.sortType === SORT_FIELD_ALPHABETICALLY,
+            })}
+            onClick={() => {
+              setSortField((prevState) => ({
+                isReversed: prevState.isReversed,
+                sortType: SORT_FIELD_ALPHABETICALLY,
+              }));
+            }}
+          >
+            Sorted alphabetically by name
+          </button>
+
+          <button
+            type='button'
+            className={cn('button', {
+              'is-info': sortField.sortType === SORT_FIELD_BY_AGE,
+            })}
+            onClick={() => {
+              setSortField((prevState) => ({
+                isReversed: prevState.isReversed,
+                sortType: SORT_FIELD_BY_AGE,
+              }));
+            }}
+          >
+            Sorted by age
+          </button>
+
+          <button
+            type='button'
+            className={cn('button', { 'is-info': sortField.isReversed })}
+            onClick={() => {
+              setSortField((prevState) => ({
+                ...prevState,
+                isReversed: !prevState.isReversed,
+              }));
+            }}
+          >
+            Reverse
+          </button>
+        </div>
+
+        <input
+          type='search'
+          value={filterField.query}
+          onChange={(event) => {
+            setFilterField({ ...filterField, query: event.target.value });
+          }}
+        />
+      </div>
+      <h1 className='title'>People table</h1>
+
+      <table className='table is-striped is-narrow'>
+        <caption>
+          {selectedPeople.map((p) => p.name).join(', ') || 'No one selected'}
+        </caption>
+
+        <thead>
+          <tr>
+            <th> </th>
+            <th>name</th>
+            <th>sex</th>
+            <th>born</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {visiblePeople.map((person) => (
+            <tr
+              key={person.slug}
+              className={isSelected(person) ? 'has-background-warning' : ''}
+            >
+              <td>
+                {isSelected(person) ? (
+                  <button
+                    type='button'
+                    className='button is-small is-rounded is-danger'
+                    onClick={() => removePerson(person)}
+                  >
+                    <span className='icon is-small'>-</span>
+                  </button>
+                ) : (
+                  <button
+                    type='button'
+                    className='button is-small is-rounded is-success'
+                    onClick={() => addPerson(person)}
+                  >
+                    <span className='icon is-small'>+</span>
+                  </button>
+                )}
+              </td>
+              <td>{person.name}</td>
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
